578180e6cb0cd89c7c67743a9b8caacc64de53c0
Jan Winkler <winkler@cs.uni-bremen.de>
2014-12-08 07:46:09 +0100

    Removed content due to repository move, put explanation into README.md
    The repository was moved to: https://github.com/code-iai/beliefstate

--- scripts/bstools/Beliefstate Tools/analyzelog.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import sys
-from LogAnalyzer import LogAnalyzer
-
-
-laAnalyzer = LogAnalyzer()
-laAnalyzer.analyzeLog(sys.argv[1])
--- scripts/bstools/Beliefstate Tools/extract_data.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import sys
-from DataExtractor import DataExtractor
-
-
-deExtractor = DataExtractor()
-deExtractor.extractData(sys.argv[1])
